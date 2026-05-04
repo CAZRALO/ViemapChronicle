7bed5364b0566a74283f14b7348a95b310cf69c6
Cazralo <cazralobo44@gmail.com>
2026-05-04 16:31:47 +0700

    up

--- app.py
+++ app.py
@@ -268,4 +268,4 @@ def chat():
         return jsonify({"response": f"Lỗi! Vui lòng thử lại sau vài phút"})
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5050)))
